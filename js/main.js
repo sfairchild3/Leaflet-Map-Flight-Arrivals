a38151a36a4fe59c434362cf316228965f18b25f
sfairchild3 <46945028+sfairchild3@users.noreply.github.com>
2019-07-01 13:10:18 -0700

    finish layout
    added zoom control
    added ranked attribute panel

--- js/main.js
+++ js/main.js
@@ -1,41 +1,51 @@
 //create main map tilelayer
 function createMap(searchLayer) {
-	var backgroundLayer = new L.StamenTileLayer("terrain");
+	var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+		subdomains: 'abcd',
+
+	});
 	var map = new L.Map("map", {
 		center: new L.LatLng(37.8, -96),
 		zoom: 5,
 		minZoom: 3,
-	});
-	map.addLayer(backgroundLayer);
+		zoomControl: false
+	})
+	map.addLayer(CartoDB_Positron);
+	map.addControl(new L.Control.ZoomMin())
 	getData(map);
-	$('#year').html("<h4>2008</h4");
 }
 
 
-function getData(map, searchLayer) {
+function getData(map) {
 	//load the data
-	$.ajax("data/AirportArrivals_percentOnTime.geojson", {
+	$.ajax("data/airports.geojson", {
 		dataType: "json",
 		success: function (response) {
 
 			//create attributes
 			var attributes = processData(response);
-			console.log(attributes)
 			createPropSymbols(response, map, attributes);
 			createSequenceControls(map, attributes);
+
 		}
 	});
 }
 
-//version using attributes of geojson response
+//style data and add search element
 function createPropSymbols(response, map, attributes) {
+
 	var searchLayer = L.geoJson(response, {
 		pointToLayer: function (feature, latlng) {
 			return pointToLayer(feature, latlng, attributes)
+		},
+		style: function (feature) {
+			return styleLayer(feature, attributes)
 		}
 	})
-
 	searchLayer.addTo(map);
+
+	//add search control feature to map
 	map.addControl(new L.Control.Search({
 		layer: searchLayer,
 		propertyName: 'Airport Name',
@@ -45,42 +55,25 @@ function createPropSymbols(response, map, attributes) {
 }
 
 
+function styleLayer(feature, attributes) {
+	var colorToUse;
+	var attribute = attributes[0];
+	var attValue = Number(feature.properties[attribute]);
+	if (attValue > 60 && attValue < 70) colorToUse = '#6CA4B0';
+	if (attValue > 70 && attValue < 80) colorToUse = '#01818E';
+	if (attValue > 80 && attValue < 90) colorToUse = '#01535E';
+	return {
+		color: colorToUse,
+		radius: calcPropRadius(attValue),
+		weight: 2
+	};
+}
 
-//function searchName(map, searchLayer) {
-//	map.addControl(new L.Control.Search({
-//		layer: searchLayer,
-//		propertyName: 'Airport Name'
-//	}))
-//
-//}
-
-
-
-
-//
-//function searchBar(response, map) {
-//	var searchLayer = L.geoJson(response, {
-//		eachLayer: getName
-//	})
-//
-//	map.addControl(new L.Control.Search({
-//		layer: searchLayer
-//	}));
-
-
-
-//function getName(feature, layer) {
-//	var name = feature.properties["Airport Name"]
-//	console.log(name)
-//	return name
-//}
-
-//calculate the radius of each proportional symbol
 
 function calcPropRadius(attValue) {
 	//scale factor to adjust symbol size evenly
 	var baseline = attValue - 60;
-	var scaleFactor = 30;
+	var scaleFactor = 20;
 	//area based on attribute value and scale factor
 	var area = baseline * scaleFactor;
 	//radius calculated based on area
@@ -97,37 +90,32 @@ function pointToLayer(feature, latlng, attributes) {
 	//create marker color
 	var geojsonMarkerOptions = {
 		radius: 8,
-		fillColor: "#53b7d2",
-		color: "#000",
 		weight: 1,
 		opacity: 1,
 		fillOpacity: 0.8
 
 	};
 
-	//create popup content to bind to marker
+	//get attribute value of airport precent arrival for popup
 	var attValue = Number(feature.properties[attribute]);
-	//    console.log(attValue)
 
+	//create popup content to bind to marker
 	var popUpContent = "<h6>" + feature.properties["Airport Name"] + "</h6><h6><b>Percent On Time:</b> " + attValue + "</h6>";
 
-	var title = feature.properties["Airport Name"]
-	//	console.log(title)
-
-
-	//use circle radius function to create proportional symbols
-	geojsonMarkerOptions.radius = calcPropRadius(attValue);
-
-	//assign location and style to leaflet circle layer
 	var layer = L.circleMarker(latlng, geojsonMarkerOptions);
 
 	//bind a tooltip to the marker layer
 	layer.bindPopup(popUpContent);
 
+	//add initial panel information on first load
+	$('#year').html('2008');
+	$("#best").html('<h5><b>Best on Time: </b> Salt Lake City International Airport 83.58 %</h5?');
+	$("#worst").html('<h5><b>Worst on Time: </b> Newark Liberty International Airport 62.32%</h5>');
+
 	// return 
 	return layer;
-
 }
+
 
 
 //create sequence control slider options
@@ -140,7 +128,7 @@ function createSequenceControls(map, attributes) {
 
 	//set slider attributes
 	$('.range-slider').attr({
-		max: 10,
+		max: 11,
 		min: 0,
 		value: 0,
 		step: 1
@@ -149,8 +137,9 @@ function createSequenceControls(map, attributes) {
 	// create slider event handler
 	$('.range-slider').on('input', function () {
 		var index = $(this).val();
+		$('#year').html(attributes[index]);
 		updatePropSymbols(map, attributes[index]);
-		updateYear(index);
+		sortData(map, attributes[index]);
 	});
 
 
@@ -159,31 +148,27 @@ function createSequenceControls(map, attributes) {
 		var index = $('.range-slider').val();
 		if ($(this).attr('id') == 'forward') {
 			index++;
-			index = index > 10 ? 0 : index;
+			index = index > 11 ? 0 : index;
 		} else if ($(this).attr('id') == 'reverse') {
 			index--;
-
-			index = index < 0 ? 10 : index;
+			index = index < 0 ? 11 : index;
 		}
-
 		$('.range-slider').val(index);
+		$('#year').html(attributes[index]);
 		updatePropSymbols(map, attributes[index]);
-		updateYear(index);
+		sortData(map, attributes[index]);
 	});
 }
 
 
-//Step 10: Resize proportional symbols according to new attribute values
 function updatePropSymbols(map, attribute) {
 	map.eachLayer(function (layer) {
 		if (layer.feature && layer.feature.properties[attribute]) {
+
 			//access feature properties
 			var props = layer.feature.properties;
 
-
-			//update each feature's radius based on new attribute values
-			var radius = calcPropRadius(props[attribute]);
-			layer.setRadius(radius);
+			var attValue = props[attribute]
 
 			//add city to popup content string
 			var popupContent = "<h6><b>" + props["Airport Name"] + "</b> </h6>";
@@ -192,47 +177,34 @@ function updatePropSymbols(map, attribute) {
 			popupContent += "<h6><b>Percent on Time: </b>" +
 				props[attribute] + "</h6>";
 
-			//replace the layer tooltip
+
+			//change map values based on year (attribute index)
+			var resetStyle = (function (layer) {
+				var colorToUse;
+				if (attValue > 60 && attValue < 70) colorToUse = '#6CA4B0';
+				if (attValue > 70 && attValue < 80) colorToUse = '#01818E';
+				if (attValue > 80 && attValue < 90) colorToUse = '#01535E';
+				return {
+					color: colorToUse,
+					radius: calcPropRadius(attValue),
+					weight: 2
+				};
+			})
+
 			layer.bindPopup(popupContent);
 
-			updateYear();
+			layer.setStyle(resetStyle(attValue));
+
 		}
 	});
 
 }
 
-function updateYear(index) {
-	if (index == 0) {
-		$('#year').html("<h4>2008</h4");
-	} else if (index == 1) {
-		$('#year').html("<h4>2009</h4");
-	} else if (index == 2) {
-		$('#year').html("<h4>2010</h4");
-	} else if (index == 3) {
-		$('#year').html("<h4>2011</h4");
-	} else if (index == 4) {
-		$('#year').html("<h4>2012</h4");
-	} else if (index == 5) {
-		$('#year').html("<h4>2013</h4");
-	} else if (index == 6) {
-		$('#year').html("<h4>2014</h4");
-	} else if (index == 7) {
-		$('#year').html("<h4>2015</h4");
-	} else if (index == 8) {
-		$('#year').html("<h4>2016</h4");
-	} else if (index == 9) {
-		$('#year').html("<h4>2017</h4");
-	} else if (index == 10) {
-		$('#year').html("<h4>2018</h4");
-	}
-}
-
-
 //build attribute arrays
 function processData(response) {
 	var attributes = [];
-	i = 0;
-	var values = response.features[i].properties;
+	//	i = 0;
+	var values = response.features[0].properties;
 	for (var item in values) {
 		if (item.indexOf("20") > -1) {
 			attributes.push(item);
@@ -240,5 +212,46 @@ function processData(response) {
 	}
 	return attributes;
 }
+
+//create arrays of attribute values to find min and max values
+function sortData(map, attribute) {
+	var ranked = []
+
+	map.eachLayer(function (layer) {
+		if (layer.feature && layer.feature.properties[attribute]) {
+
+			//access feature properties
+			var props = layer.feature.properties;
+			var attValue = props[attribute];
+			ranked.push(attValue);
+		}
+	})
+	var max = Math.max.apply(Math, ranked);
+	var min = Math.min.apply(Math, ranked);
+	displayRanks(map, attribute, max, min)
+}
+
+
+//display min and max values in panel
+function displayRanks(map, attribute, max, min) {
+	map.eachLayer(function (layer) {
+		if (layer.feature && layer.feature.properties[attribute]) {
+
+			//access feature properties
+			var props = layer.feature.properties;
+			var attValue = props[attribute];
+
+			//match min and max value from sortData in panel 
+			if (max == attValue) {
+				$("#best").html("<h5><b>Best on Time: </b>" + props['Airport Name'] + ' ' + max + '%</h5>')
+			}
+			if (min == attValue) {
+				$("#worst").html("<h5><b>Worst on Time: </b>" + props['Airport Name'] + ' ' + min + '%</h5>')
+			}
+
+		}
+	})
+}
+
 
 $(document).ready(createMap);
